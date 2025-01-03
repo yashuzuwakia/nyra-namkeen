29bb88ceaddb6d8d2d33662d13d0becde81fe136
Yash <iam.007yash@gmail.com>
2025-01-03 22:26:44 +0530

    Adding elements

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Nyra Namkeen</h1>
+      <p>Taste Nahi, Feelings Hai!</p>
+    </div>
+  );
+};
+
+export default Home;
