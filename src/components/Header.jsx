b2836921db308d633d17329de1c275e8a80f4a07
Yash <iam.007yash@gmail.com>
2025-01-04 00:29:20 +0530

    added responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import cover from "../assets/images/homehead1.png"
-import logo from "../assets/images/logo.png"
+import cover from "../assets/images/homehead1.png";
+import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header className="header">
       <nav className="nav">
         <Link to="/home" className="title">
-            <img src={logo} alt="Nyra Namkeen" className="logo" />
+          <img src={logo} alt="Nyra Namkeen" className="logo" />
         </Link>
-        <ul>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={menuActive ? "active" : ""}>
           <li>
             <Link to="/products">Products</Link>
           </li>
@@ -23,7 +33,7 @@ const Header = () => {
         </ul>
       </nav>
       <div>
-        <img src={cover} alt="" />
+        <img src={cover} alt="Home Cover" />
       </div>
     </header>
   );
