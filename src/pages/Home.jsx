da50aeccdecc4d96c30ffb5abba361432cc65a1a
Yash <iam.007yash@gmail.com>
2025-01-04 23:15:11 +0530

    made content at center of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/images/logo.png'
+import cover from '../assets/images/homehead1.png'
 import "./Home.css"
 
 const Home = () => {
   return (
     <div className="home">
       <img src={logo} alt="" className='logo'/>
+      <img src={cover} alt="" className='cover'/>
       <h1>Welcome to Nyra Namkeen</h1>
       <p>Taste Nahi, Feelings Hai!</p>
     </div>
