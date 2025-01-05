96df24199a670584c794c118596f79e50db7ab9c
Yash <iam.007yash@gmail.com>
2025-01-05 11:03:28 +0530

    fix home route

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/products" element={<Products/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
