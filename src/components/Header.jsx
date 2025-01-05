cedfa92dc74779831f4cd40d9896fbb6b75176fb
Yash <iam.007yash@gmail.com>
2025-01-05 11:33:18 +0530

    handle outside menu click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Header.css";
 import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
+  const menyRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuActive(!menuActive);
   };
+
+  useEffect(()=>{
+    const handleClickOutside = (event) =>{
+      if(menyRef.current && !menyRef.current.contains(EventTarget.target)){
+        setMenuActive(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [])
 
   return (
     <header className="header">
@@ -19,7 +33,7 @@ const Header = () => {
         <button className="menu-toggle" onClick={toggleMenu}>
           ☰
         </button>
-        <ul className={menuActive ? "active" : ""}>
+        <ul className={menuActive ? "active" : ""} ref={menyRef}>
           <li>
             <Link to="/products">Products</Link>
           </li>
